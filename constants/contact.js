c9e4703fbea3266944252e4532552498f6c94428
uzair1707 <uzair1707>
2023-10-30 18:46:20 +0500

    Changed the contact

--- constants/contact.js
+++ constants/contact.js
@@ -1,5 +1,5 @@
 const EMAIL_ID = 'funncartstore@gmail.com';
-const PHONE_NO = '+923316442619';
+const PHONE_NO = '+92-3343866227';
 const ADDRESS = '545 Mavis Island Chicago, IL 99191';
 
 export { EMAIL_ID, PHONE_NO, ADDRESS };
