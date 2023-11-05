6dc06a86cc69b6b72252004d82ad36103fd41441
uzair1707 <uzair1707>
2023-11-05 19:51:15 +0500

    changed testimonial component

--- components/Cards/Testimonial.jsx
+++ components/Cards/Testimonial.jsx
@@ -2,16 +2,16 @@ import Image from 'next/image';
 
 export default function Testimonial({ message, name, media }) {
   return (
-    <div className="flex bg-white h-[200px] rounded-2xl">
-      <div className="py-10 pl-6 pr-8 border-r border-[#D4D4D4] w-[300px]">
-        <p className="overflow-hidden text-sm text-body max-h-20">{message}</p>
+    <div className="flex bg-white  rounded-2xl">
+      <div className="py-10 pl-6 pr-8 border-r border-[rgb(234,234,234)] w-[400px]">
+        <p className="overflow-hidden text-[11px] text-body max-h-20">{message}</p>
         <p className="pt-6 text-sm font-medium text-heading">{name}</p>
       </div>
-      <div className="p-8 w-[160px] flex items-center justify-center">
+      <div className="flex items-center justify-center">
         <img
           src={media[0].original_url}
           alt={name}
-          className="w-20 h-20 rounded-full"
+          className="w-60 h-40 rounded-md"
           draggable={false}
         />
       </div>
